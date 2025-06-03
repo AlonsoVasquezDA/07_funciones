83a38d2c1cdc7510959ea2d5cb8184cced14ed1a
AlonsoVasquezDA <alonsovasquez1409@gmail.com>
2025-06-02 21:50:47 -0500

    Actualizacion del ejercicio2:Mejora del texto

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -1,31 +1,31 @@
 #include<iostream>
 using namespace std;
-void Divresta(int,int,int,int);
+void Dividirporesta(int,int,int,int);
 int main(){
-	int num1,num2,cociente,resto;
+	int n1,n2,cociente,resto;
 	cout<<"Ingrese el primer numero: ";
-	cin>>num1;
+	cin>>n1;
 	cout<<"Ingrese el segundo numero: ";
-	cin>>num2;
-	if(num1>0 && num2>0){
-		Divresta(num1,num2,cociente,resto);
+	cin>>n2;
+	if(n1>0 && n2>0){
+		Dividirporesta(n1,n2,cociente,resto);
 		cout<<"El cociente es: "<<cociente<<endl;
 		cout<<"El resto es: "<<resto<<endl;
 	}
 	else{
-		cout<<"Ambos numeros deben ser positivos mayores que cero."<<endl;
+		cout<<"ERROR: Ambos numeros deben ser positivos mayores que cero."<<endl;
 	}
 	return 0;
 }
-void Divresta(int num1, int num2, int ccociente, int rresto){
+void Dividirporesta(int n1, int n2, int ccociente, int rresto){
 	int mayor,menor;
-	if(num1>num2){
-		mayor=num1;
-		menor=num2;
+	if(n1>n2){
+		mayor=n1;
+		menor=n2;
 	}
 	else{
-	    mayor=num2;
-		menor=num1;
+	    mayor=n2;
+		menor=n1;
 	}
 	ccociente=0;
 	rresto=mayor;
